abd040bd493e659ca86c4a66b166af27eecab14a
Misterguppy <emetpark@gmail.com>
2024-05-15 14:55:45 -0400

    Update RunEmployee.java

--- EMPLOY/RunEmployee.java
+++ EMPLOY/RunEmployee.java
@@ -5,15 +5,15 @@ public class RunEmployee {
         Employee e = new Employee(40000.0);
         e.Increase(2000.0);
         double p = e.getPay();
-        System.out.println("Salary is" + p);
+        System.out.println("Salary is " + p);
 
         e.Increase(-500.0);
         p = e.getPay();
-        System.out.println("Salary is" + p);
+        System.out.println("Salary is " + p);
 
         e.Increase(3000.0);
         p = e.getPay();
-        System.out.println("Salary is" + p);
+        System.out.println("Salary is " + p);
 
     }
 }
